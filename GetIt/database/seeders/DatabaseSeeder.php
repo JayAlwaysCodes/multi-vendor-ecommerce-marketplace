8f5a14e237a4088e37673bd7aee545eeebd208d8
jayalwayscodes <johnsonjardocs@gmail.com>
2025-04-22 18:01:58 +0100

    created a couple of seeders and enums

--- GetIt/database/seeders/DatabaseSeeder.php
+++ GetIt/database/seeders/DatabaseSeeder.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 namespace Database\Seeders;
 
@@ -13,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call([
+            RoleS eder::class,
+            UserSeedr::class,
+        ])
     }
 }
