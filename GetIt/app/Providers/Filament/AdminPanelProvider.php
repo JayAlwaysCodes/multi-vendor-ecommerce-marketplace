4ca1e2d8d659400b94e3d540a0b9b509309af697
jayalwayscodes <johnsonjardocs@gmail.com>
2025-04-23 13:11:55 +0100

    created department resource

--- GetIt/app/Providers/Filament/AdminPanelProvider.php
+++ GetIt/app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use Illuminate\Database\Eloquent\Model;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -62,5 +63,9 @@ class AdminPanelProvider extends PanelProvider
             //     Authenticate::class,
             // ])
             ;
+    }
+
+    public function boot(){
+        Model::unguard();        
     }
 }
